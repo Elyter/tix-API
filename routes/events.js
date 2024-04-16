1748964e394021b46042cdfc7e5d664d1002c70e
Elyter <eliottbreg@gmail.com>
2024-04-16 16:34:55 +0200

    Add research event by id

--- routes/events.js
+++ routes/events.js
@@ -27,4 +27,27 @@ router.get('/events/', function(req, res) {
     });
 });
 
+router.get('/events/:id', function(req, res) {
+    res.header('Content-type', 'application/json');
+    res.header('Access-Control-Allow-Origin', "*");
+
+    const id = req.params.id;
+
+    sql.query("SELECT * FROM events WHERE id = ?", [id], function(err, result) {
+        if (err) {
+            console.log(err);
+            res.status(500).send({ error: 'Erreur lors de la récupération de l\'évènement' });
+            return;
+        }
+        if (result.length > 0) {
+            // Formater la date pour l'événement
+            result[0].date = moment(result[0].date).format('dddd DD MMMM [à] HH:mm');
+            result[0].imageUrl = `/images/events/${id}.jpeg`;
+            res.status(200).send(result[0]);
+            return;
+        }
+        res.status(404).send({ error: 'Événement non trouvé' });
+    });
+});
+
 module.exports = router;
