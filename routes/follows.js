e43fb24947a0b1a42af36825f06cd87e029a93c0
Elyter <eliottbreg@gmail.com>
2024-05-13 01:38:45 +0200

    follow gestion

--- routes/follows.js
+++ routes/follows.js
@@ -57,4 +57,45 @@ router.post('/follows/:uid/:id', function(req, res) {
     });
 });
 
+// endpoint pour récupérer le nom et la pp de chaque followers d'un organisateur
+// router.get('/follows/followers/:id', function(req, res) {
+//     res.header('Content-type', 'application/json');
+//     res.header('Access-Control-Allow-Origin', "*");
+
+//     const id = req.params.id;
+
+//     sql.query("SELECT users.uid, users.firstName, users.lastName, users.pp FROM users INNER JOIN follows ON users.uid = follows.uid WHERE idOrganizer = ?", [id], function(err, result) {
+//         if (err) {
+//             console.log(err);
+//             res.status(500).send({ error: 'Erreur lors de la récupération des followers' });
+//             return;
+//         }
+//         if (result.length > 0) {
+//             res.status(200).send(result);
+//             return;
+//         }
+//     });
+// });
+
+// endpoint pour récupérer le nom et la pp de chaque organisteurs que follow un utilisateur
+router.get('/follows/following/:uid', function(req, res) {
+    res.header('Content-type', 'application/json');
+    res.header('Access-Control-Allow-Origin', "*");
+
+    const uid = req.params.uid;
+
+    sql.query("SELECT organizer.id, organizer.name, organizer.pp FROM organizer INNER JOIN follows ON organizer.id = follows.idOrganizer WHERE follows.uid = ?", [uid], function(err, result) {
+        if (err) {
+            console.log(err);
+            res.status(500).send({ error: 'Erreur lors de la récupération des following' });
+            return;
+        }
+        if (result.length > 0) {
+            res.status(200).send(result);
+            return;
+        }
+    });
+});
+
+
 module.exports = router;
